0cca09f9725894eda2d450a2473fb75b63bd0b39
jardondaful <68207907+jardondaful@users.noreply.github.com>
2022-05-02 21:19:16 -0700

    made links at top of page appear to be interactive buttons, changed size, color, and font of all text, made images appear interactive, created heading color

--- bites-frontend/src/pages/Map.js
+++ bites-frontend/src/pages/Map.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Map() {
   return (
     <div>
-      <h1>Map Page for Bruin Bites</h1>
+      <h1>Map Page for Bruin Bites!</h1>
     </div>
   );
 }
