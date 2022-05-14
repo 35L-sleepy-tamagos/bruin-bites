e08a796bf9f391d410f8723301734a8fe0031f4b
ehg <hwang.edward8@gmail.com>
2022-05-13 18:52:00 -0700

    Navbar.js: Added Meal Period to NavBar

--- bites-frontend/src/components/Navbar.js
+++ bites-frontend/src/components/Navbar.js
@@ -3,6 +3,55 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  let mealPeriod = "None Currently";
+  let d = new Date();
+
+  let hours = d.getHours();
+  let mins = d.getMinutes();
+
+  if (hours >= 7 && hours < 10) {
+    mealPeriod = "Breakfast";
+  }
+
+  if (hours >= 11 && hours < 15) {
+    mealPeriod = "Lunch/Brunch";
+  }
+
+  if (hours >= 17 && hours < 21) {
+    mealPeriod = "Dinner";
+  }
+
+  if (hours >= 21) {
+    mealPeriod = "Extended Dinner";
+  }
+
+  if (hours >= 10 && hours < 11) {
+    let remainingHours = 11 - (hours + 1);
+    remainingHours = "0" + remainingHours;
+    let remainingMinutes = 60 - mins;
+    if (remainingMinutes < 10) {
+      remainingMinutes = "0" + remainingMinutes;
+    }
+    mealPeriod = remainingHours + ":" + remainingMinutes + " until Lunch";
+  }
+
+  if (hours >= 15 && hours < 17) {
+    let remainingHours = 17 - (hours + 1);
+    remainingHours = "0" + remainingHours;
+    let remainingMinutes = 60 - mins;
+    mealPeriod = remainingHours + ":" + remainingMinutes + " until Dinner";
+  }
+
+  if (hours < 7) {
+    let remainingHours = 7 - (hours + 1);
+    remainingHours = "0" + remainingHours;
+    let remainingMinutes = 60 - mins;
+    if (remainingMinutes < 10) {
+      remainingMinutes = "0" + remainingMinutes;
+    }
+    mealPeriod = remainingHours + ":" + remainingMinutes + " until Breakfast";
+  }
+
   return (
     <div className="nav-bg">
       <li>
@@ -17,6 +66,9 @@ const Navbar = () => {
       <li>
         <Link to="/profile" style={{ textDecoration: 'none', color: 'white' }}>Profile</Link>
       </li>
+      <div className="Meal-Period" style={{ textDecoration: 'none', color: 'white' }}>
+        { mealPeriod }
+      </div>
     </div>
   );
 };
