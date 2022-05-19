0c9bce808be5299be9704b12365639b04c5e4f8b
ehg <hwang.edward8@gmail.com>
2022-05-19 02:09:43 -0700

    Navbar.js: Made the time update dynamically

--- bites-frontend/src/components/Navbar.js
+++ bites-frontend/src/components/Navbar.js
@@ -1,56 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  let mealPeriod = "None Currently";
-  let d = new Date();
+  let [mealPeriod, setPeriod] = useState("None Currently")
+  let d;
+  let hours;
+  let mins; 
+  let seconds;
+  let remainingHours;
+  let remainingMinutes;
+  let remainingSeconds;
 
-  let hours = d.getHours();
-  let mins = d.getMinutes();
-
-  if (hours >= 7 && hours < 10) {
-    mealPeriod = "Currently Breakfast";
-  }
-
-  if (hours >= 11 && hours < 15) {
-    mealPeriod = "Currently Lunch/Brunch";
-  }
-
-  if (hours >= 17 && hours < 21) {
-    mealPeriod = "Currently Dinner";
-  }
-
-  if (hours >= 21) {
-    mealPeriod = "Currently Extended Dinner";
-  }
-
-  if (hours >= 10 && hours < 11) {
-    let remainingHours = 11 - (hours + 1);
-    remainingHours = "0" + remainingHours;
-    let remainingMinutes = 60 - mins;
-    if (remainingMinutes < 10) {
-      remainingMinutes = "0" + remainingMinutes;
+  function showDate() {
+    d = new Date();
+  
+    hours = d.getHours();
+    mins = d.getMinutes();
+    seconds = d.getSeconds();
+  
+    if (hours >= 7 && hours < 10) {
+      mealPeriod = "Currently Breakfast";
     }
-    mealPeriod = remainingHours + ":" + remainingMinutes + " until Lunch";
-  }
-
-  if (hours >= 15 && hours < 17) {
-    let remainingHours = 17 - (hours + 1);
-    remainingHours = "0" + remainingHours;
-    let remainingMinutes = 60 - mins;
-    mealPeriod = remainingHours + ":" + remainingMinutes + " until Dinner";
-  }
-
-  if (hours < 7) {
-    let remainingHours = 7 - (hours + 1);
-    remainingHours = "0" + remainingHours;
-    let remainingMinutes = 60 - mins;
-    if (remainingMinutes < 10) {
-      remainingMinutes = "0" + remainingMinutes;
+  
+    if (hours >= 11 && hours < 15) {
+      mealPeriod = "Currently Lunch/Brunch";
     }
-    mealPeriod = remainingHours + ":" + remainingMinutes + " until Breakfast";
+  
+    if (hours >= 17 && hours < 21) {
+      mealPeriod = "Currently Dinner";
+    }
+  
+    if (hours >= 21) {
+      mealPeriod = "Currently Extended Dinner";
+    }
+  
+    if (hours >= 10 && hours < 11) {
+      remainingHours = 11 - (hours + 1);
+      remainingHours = "0" + remainingHours;
+      remainingMinutes = 60 - mins;
+      if (remainingMinutes < 10) {
+        remainingMinutes = "0" + remainingMinutes;
+      }
+      mealPeriod = remainingHours + ":" + remainingMinutes + " until Lunch";
+    }
+  
+    if (hours >= 15 && hours < 17) {
+      remainingHours = 17 - (hours + 1);
+      remainingHours = "0" + remainingHours;
+      remainingMinutes = 60 - mins;
+      remainingSeconds = 60 - seconds;
+      if (remainingMinutes < 10) {
+        remainingMinutes = "0" + remainingMinutes;
+      }
+      if (remainingMinutes === 60) {
+        remainingMinutes = "00";
+        remainingHours += 1;
+      }
+      if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+      }
+      if (remainingSeconds === 60) {
+        remainingSeconds = "00";
+        remainingMinutes += 1;
+      }
+      mealPeriod = remainingHours + ":" + remainingMinutes + ":" + remainingSeconds + " until Dinner";
+    }
+  
+    if (hours < 7) {
+      let remainingHours = 7 - (hours + 1);
+      remainingHours = "0" + remainingHours;
+      let remainingMinutes = 60 - mins;
+      remainingSeconds = 60 - seconds;
+      if (remainingMinutes < 10) {
+        remainingMinutes = "0" + remainingMinutes;
+      }
+      if (remainingMinutes === 60) {
+        remainingMinutes = "00";
+        remainingHours += 1;
+      }
+      if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+      }
+      if (remainingSeconds === 60) {
+        remainingSeconds = "00";
+        remainingMinutes += 1;
+      }
+      mealPeriod = remainingHours + ":" + remainingMinutes + ":" + remainingSeconds + " until Breakfast";
+      setPeriod(mealPeriod);
+    }
   }
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      showDate();
+    }, 500);
+    
+    return () => clearInterval(interval);
+  });
 
   return (
     <div className="nav-bg">
