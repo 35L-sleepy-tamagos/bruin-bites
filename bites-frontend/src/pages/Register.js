78c9315df88a84e49ccdadec8724b50ed59dce62
Derek Jiang <derek.l.jiang@gmail.com>
2022-05-23 23:47:31 -0700

    bootstrap for Register and mobile support

--- bites-frontend/src/pages/Register.js
+++ bites-frontend/src/pages/Register.js
@@ -1,10 +1,9 @@
-import "../css/SignIn.css";
-
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { emailRegister, googleSignIn } from "../components/firebaseConfig/utils";
 import { auth } from "../components/firebaseConfig/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { Container, Row, Col, Form, Card, Button } from "react-bootstrap";
 
 export default function Register() {
 	const [email, setEmail] = useState("");
@@ -39,32 +38,59 @@ export default function Register() {
 	};
 
 	return (
-		<div>
-			<h1>Register for an Account!</h1>
-			<div className="centered">
-				<div className="signin">
-					<input type="text" className="signin-input"
+		<Container>
+			<Row className="mb-3">
+				<Col>
+					<h1>Register for an Account!</h1>
+				</Col>
+			</Row>
+			<Row className="justify-content-md-center">
+				<Col className="col-12 col-md-6">
+				<Card className="p-3">
+				<Card.Body className="text-center">
+					<Form className="mb-3">
+					<Form.Group className="mb-3">
+					<Form.Control type="text" className="signin-input"
 						value={ name } onChange={ (e) => setName(e.target.value) }
-						placeholder="Input your Name" />
-					<input type="text" className="signin-input" 
-						value={ email } onChange={ (e) => setEmail(e.target.value) } 
-						placeholder="Input your Email" />
-					<input type="password" className="signin-input"
-						value={ password } onChange={ (e) => setPassword(e.target.value) }
-						placeholder="Input your Password" />
-					<button className="signin-button"
-						onClick={ () => register(name, email, password) }>
-						Register Now!
-					</button>
-					<button className="signin-button signin-google"
-						onClick={ () => googleSignIn() }>
-						Register with Google!
-					</button>
-					<div>
-						<Link to="/signin">Already Have an Account? Sign In Here!</Link>
-					</div>
-				</div>
-			</div>
-		</div>
+						placeholder="Input your Name" 
+					/>
+					</Form.Group>
+					<Form.Group className="mb-3">
+					<Form.Control
+						type="text" className="signin-input"
+						value={ email } onChange={ (e) => setEmail(e.target.value) }
+						placeholder="Input your Email" 
+					/>
+					</Form.Group>
+					<Form.Group className="mb-3">
+						<Form.Control 
+							type="password" className="signin-input"
+							value={ password } onChange={ (e) => setPassword(e.target.value) }
+							placeholder="Input your Password" 
+						/>
+					</Form.Group>
+					<Form.Group className="d-grid mb-3">
+						<Button className=""
+							variant="primary"
+							size="lg"
+							onClick={ () => register(name, email, password)}>
+							Register Now!
+						</Button>
+					</Form.Group>
+					<Form.Group className="d-grid">
+						<Button className=""
+							variant="danger"
+							size="lg"
+							onClick={ () => googleSignIn() }>
+							Register with Google!
+						</Button>
+					</Form.Group>
+					</Form>
+					<Link to="/signin">Already Have an Account? Sign In Here!</Link>
+				</Card.Body>
+				</Card>
+				</Col>
+			</Row>
+		</Container>
 	)
 }
