1761c60667a620b5f080866fd22b0fb631923198
ehg <hwang.edward8@gmail.com>
2022-06-01 16:55:06 -0700

    changed the image that loads for local users

--- bites-frontend/src/components/Navbar.js
+++ bites-frontend/src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = ({ user }) => {
         setProfileImage(user.photoURL);
       }
       else {
-        setProfileImage(localProfile);
+        setProfileImage(ProfileImage);
       }
     })
   }, [])
